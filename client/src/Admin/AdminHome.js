580301492ee5d0bee430cc42e8f7cffb4a51e0cc
PGAvanthika <avanthikapg22@gmail.com>
2025-05-27 13:21:09 +0530

    Updated Routing and admin navgation

---
+++ client/src/Admin/AdminHome.js
@@ -0,0 +1,65 @@
+import React from "react";
+import "./AdminHome.css";
+import UserCard from "../Components/UserCard";
+
+
+const dummyUsers = [
+  {
+    name: "Yuthish",
+    role: "cybersecurity enthusiast",
+    imageSrc: "/images/user1.jpg",
+  },
+  {
+    name: "Avanthika PG",
+    role: "DevOps enthusiast",
+    imageSrc: "/images/user2.jpg",
+  },
+  { name: "Shreya Sri", role: "Team Lead", imageSrc: "/images/user3.jpg" },
+  // Add more dummy users as needed
+];
+
+const AdminHome = () => {
+  return (
+    <div className="admin-home">
+      <header className="top-nav">
+        <button className="profile-btn">
+          <ion-icon name="person-circle-outline"></ion-icon>
+        </button>
+        <nav>
+          <a href="#">Home</a>
+          <a href="#">View Logs</a>
+        </nav>
+        <button className="logout-btn">
+          <ion-icon name="power-outline"></ion-icon>
+        </button>
+      </header>
+
+      
+
+      <div className="search-bar">
+        <div className="search-input-wrapper">
+          <ion-icon name="search-outline" class="search-icon"></ion-icon>
+          <input type="text" placeholder="Search" />
+        </div>
+        <button className="filter-btn">Filter</button>
+        <button className="add-user-btn">
+          <ion-icon name="add-outline" class="plus-icon"></ion-icon>
+          Add new user
+        </button>
+      </div>
+
+      <div className="user-grid">
+        {dummyUsers.map((user, index) => (
+          <UserCard
+            key={index}
+            name={user.name}
+            role={user.role}
+            imageSrc={user.imageSrc}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default AdminHome;
